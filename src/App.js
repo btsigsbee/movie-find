b46efab4312f250c8bfdf8e43d014d546fde6ea4
Bryce Sigsbee <btsigsbee@gmail.com>
2020-05-06 13:05:05 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     
-      <Bar/>
+      <Bar className='container'/>
       
     
   );
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Button, Col, Row} from 'react-bootstrap';
+import {Button, Col, Row, Badge} from 'react-bootstrap';
 import axios from 'axios';
+import '../App.css';
 import _ from 'lodash';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {IoIosTrash} from 'react-icons/io';
@@ -8,10 +9,11 @@ const MovieResults=(props)=>{
     const movies = props;
     const results = movies.props.map((mr) =>
     
-        <Row className='row rounded container justify-content-center border border-warning offset-3 mt-4 mr-4' key={mr.imdbID}>
-            <Col className='col-5'><img  fluid className='w-50'src={mr.Poster} alt={mr.Poster}/></Col>
-            <Col className='col-4 my-4 py-5'><Button className='btn-danger btn-block 'onClick={() => props.removeItem(mr.imdbID)}><IoIosTrash></IoIosTrash></Button>
-            <h5 className='text-light'>{mr.Title}<p>({mr.Year})</p></h5>
+        <Row className='row col-6 offset-3 rounded container justify-content-center border border-warning mt-4' key={mr.imdbID}>
+            <Col className='col-6 py-4'><img  fluid className='w-50'src={mr.Poster} alt='Poster not Available'/></Col>
+            <Col className='col-6 my-4 '><Button className='btn-danger btn-block my-1'onClick={() => props.removeItem(mr.imdbID)}><h4>Remove<IoIosTrash size={26}></IoIosTrash></h4></Button>
+            <h5 className='text-warning col-12'>{mr.Title}<h5 className='text-light'>({mr.Year})</h5></h5>
+            <Badge>{mr.Ratings}</Badge>
             
             </Col>
             
@@ -83,8 +85,8 @@ class searchBar extends React.Component{
 
         return<div className='pl-3 bg-dark'>
             <Row className='justify-content-center col-md-12 pt-3 bg-dark'>
-                <input className="col-md-4 input-group-text input-group-lg mr-4" type="text" placeholder='search for movies...' onChange={this.handleChange} />
-                <Button className='btn btn-size btn-outline-warning btn-transparent' 
+                <input className="col-4 input-group-text input-group-lg " type="text" placeholder='search for movies...' onChange={this.handleChange} />
+                <Button type='submit'className='btn btn-size mx-3 btn-outline-warning btn-transparent' 
                 onClick={this.handleSubmit}>Search</Button>
             </Row>
             <div>
