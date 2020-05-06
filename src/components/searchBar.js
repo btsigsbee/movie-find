f1176a1e2e0eb14dddac6dc7f0eee92408734519
Bryce Sigsbee <btsigsbee@gmail.com>
2020-05-06 10:21:27 -0400

    styling

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -3,14 +3,16 @@ import {Button, Col, Row} from 'react-bootstrap';
 import axios from 'axios';
 import _ from 'lodash';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {IoIosTrash} from 'react-icons/io';
 const MovieResults=(props)=>{
     const movies = props;
     const results = movies.props.map((mr) =>
     
-        <Row key={mr.imdbID}>
-            <Col className='container'><img className='w-25'src={mr.Poster} alt={mr.Poster}/></Col>
-            <Col><h6>{mr.Title}<p>({mr.Year})</p></h6>
-            <Button onClick={() => props.removeItem(mr.imdbID)}>Delete</Button>
+        <Row className='row rounded container justify-content-center border border-warning offset-3 mt-4 mr-4' key={mr.imdbID}>
+            <Col className='col-5'><img  fluid className='w-50'src={mr.Poster} alt={mr.Poster}/></Col>
+            <Col className='col-4 my-4 py-5'><Button className='btn-danger btn-block 'onClick={() => props.removeItem(mr.imdbID)}><IoIosTrash></IoIosTrash></Button>
+            <h5 className='text-light'>{mr.Title}<p>({mr.Year})</p></h5>
+            
             </Col>
             
         </Row>
@@ -82,7 +84,7 @@ class searchBar extends React.Component{
         return<div className='pl-3 bg-dark'>
             <Row className='justify-content-center col-md-12 pt-3 bg-dark'>
                 <input className="col-md-4 input-group-text input-group-lg mr-4" type="text" placeholder='search for movies...' onChange={this.handleChange} />
-                <Button className='btn btn-size' 
+                <Button className='btn btn-size btn-outline-warning btn-transparent' 
                 onClick={this.handleSubmit}>Search</Button>
             </Row>
             <div>
