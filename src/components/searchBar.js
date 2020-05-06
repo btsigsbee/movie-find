b428f364494dabfed9529a4a98eaf8333fb237d4
Bryce Sigsbee <btsigsbee@gmail.com>
2020-05-06 17:27:41 -0400

    last style changes

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -12,7 +12,7 @@ const MovieResults=(props)=>{
         <Row className='row col-10 col-sm-6 offset-1 offset-sm-3 rounded container justify-content-center border border-warning mt-4' key={mr.imdbID}>
             <Col className='col-10 col-sm-6 py-4'><img className='text-light img-fluid w-75'src={mr.Poster} alt='Poster not available'/></Col>
             <Col className='col-10  col-sm-6 my-4 container'><h5 className='text-warning col-12'>{mr.Title}<h5 className='text-light'>({mr.Year})</h5></h5>
-            <Button  fluid className='btn container btn-danger btn-block ml-3 my-1 w-75'onClick={() => props.removeItem(mr.imdbID)}><p>Remove<IoIosTrash size={26}></IoIosTrash></p></Button>
+            <Button  fluid className='btn container btn-danger btn-block ml-3 my-1 w-75'onClick={() => props.removeItem(mr.imdbID)}><p className='font-weight-bold font-sz'>Remove<IoIosTrash size={26}></IoIosTrash></p></Button>
             
             
             </Col>
@@ -83,13 +83,13 @@ class searchBar extends React.Component{
     
     render(){
 
-        return<div className='pl-3 bg-dark'>
+        return<div className='pl-3 bg-dark justify-content-center'>
             <Row className='justify-content-center col-md-12 pt-3 bg-dark'>
                 <input className="col-6 input-group-text input-group-lg " type="text" placeholder='search for movies...' onChange={this.handleChange} />
                 <Button type='submit'className='btn btn-size mx-3 btn-outline-warning btn-transparent' 
                 onClick={this.handleSubmit}>Search</Button>
             </Row>
-            <div>
+            <div className='col-12'>
                 <MovieResults props={this.state.searchResults} removeItem={this.removeItem.bind(this)}/>
             </div>
             
